07089a7a808537518ec7712d3914b7a54c4f3abe
Diego Solis <disc3110@gmail.com>
2021-09-25 22:08:54 -0500

    rules

--- src/pages/Invitees.js
+++ src/pages/Invitees.js
@@ -4,11 +4,30 @@ import { listInvitados } from '../graphql/queries';
 const Invitees = () => {
   const getInvitees = async () => {
   const todos = await API.graphql(graphqlOperation(listInvitados));
-  console.log(todos)
+  const confirmados = todos.data.listInvitados.items
+  console.log(confirmados)
 }
-  getInvitees()
+
+ const renderInvitees = (invitadosConf) => invitadosConf.map((invitee) => (
+   <tr>
+     <th scope="col">{invitee.name}</th>
+     <th scope="col">{invitee.contact}</th>
+     <th scope="col">{invitee.people}</th>
+   </tr>
+ ))
+
+ getInvitees()
   return(
-    <div>Hi</div>
+    <table className="table">
+      <thead>
+      <tr>
+        <th scope="col">Name</th>
+        <th scope="col">Contact</th>
+        <th scope="col">People</th>
+      </tr>
+      </thead>
+      <tbody></tbody>
+    </table>
   )
 }
 
--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -5,6 +5,12 @@ const Rules = () => (
     <div className="d-flex flex-column align-items-center my-4 justify-content-center">
     <img  src={rulesimg} alt="" className="imgs"/>
   </div>
+  <ul>
+    <li>No se podra entrar con ningun tipo de arma u objetos punzocortantes</li>
+    <li>No se permitirá estar afuera de la casa </li>
+    <li>Sólo se permitirá el acceso a personas que no estén registradas en esta página</li>
+    <li>El acceso y salida será controlado</li>
+  </ul>
   </>
 );
 
